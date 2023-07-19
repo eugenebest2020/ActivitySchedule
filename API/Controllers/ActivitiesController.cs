09a0a0f45161754784c8b8a93561c21d68e22a7e
eugenebest2020 <eugenebest2020@outlook.com>
2023-07-19 14:49:00 +0100

    Completed Identity Server Side

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,12 +1,14 @@
 ﻿using Application.Activities;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
 
@@ -15,7 +17,7 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new List.Query()));
         }
-
+       
         [HttpGet("{id}")]
         public async Task<IActionResult> GetActivity(Guid id)
         {
